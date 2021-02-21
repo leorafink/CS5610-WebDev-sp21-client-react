d1d032d32aa8d323189d97c00b7828b95e82a192
leorafink1 <fink.le@husky.neu.edu>
2021-02-21 17:14:35 -0500

    arrow back to courses

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import CourseManager from "./components/course-manager";
 import CourseEditor from "./components/course-editor";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container-fluid">
-        <CourseManager/>
-        <CourseEditor/>
-    </div>
+      <BrowserRouter>
+          <Route path="/courses" component={CourseManager}/>
+          <Route path="/editor" component={CourseEditor}/>
+        {/*<div className="container-fluid">
+            <CourseManager/>
+            <CourseEditor/>
+        </div>*/}
+      </BrowserRouter>
   );
 }
 
--- src/components/course-row.js
+++ src/components/course-row.js
@@ -1,8 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const CourseRow = ({course}) =>
     <tr>
-        <td>{course.title}</td>
+        <td>
+            <Link to="/editor">
+                {course.title}
+            </Link>
+        </td>
         <td>{course.owner}</td>
         <td>{course.lastModified}</td>
         <td>
