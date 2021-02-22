49dad2fd439740e86485f7fd6639514bc4cc6b8f
leorafink1 <fink.le@husky.neu.edu>
2021-02-21 23:15:17 -0500

    editing and check toggle

--- src/components/course-row.js
+++ src/components/course-row.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 const CourseRow = ({
                        course,
                        deleteCourse
-                    }) =>
-    <tr>
+                    }) => {
+
+    const [editing, setEditing] = useState(false)
+    return (<tr>
         <td>
-            <Link to="/editor">
-                {course.title}
-            </Link>
+            {
+                !editing &&
+                <Link to="/editor">
+                    {course.title}
+                </Link>
+            }
+            {
+                editing &&
+                <input value={course.title}/>
+            }
         </td>
         <td>{course.owner}</td>
         <td>{course.lastModified}</td>
         <td>
             <i onClick={() => deleteCourse(course)} className="fas fa-trash"></i>
-            <i className="fas fa-edit"></i>
-            <i className="fas fa-check"></i>
+
+            {
+                editing &&
+                <i onClick={() => setEditing(false)} className="fas fa-check"></i>
+            }
+
+            {
+                !editing &&
+                <i onClick={() => setEditing(true)} className="fas fa-edit"></i>
+            }
+
+
         </td>
-    </tr>
+    </tr>)
+}
 
 export default CourseRow
