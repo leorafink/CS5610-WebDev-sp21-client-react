4737209e6e364453739edfc191f0065bc7948389
leorafink1 <fink.le@husky.neu.edu>
2021-02-22 13:32:14 -0500

    added homepage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import CourseManager from "./components/course-manager";
 import CourseEditor from "./components/course-editor";
 import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/home.js"
 
 function App() {
   return (
       <BrowserRouter>
+          <div className="container-fluid">
+          <Route path="/" exact={true}  component={Home}/>
           <Route path="/courses" component={CourseManager}/>
           {/*<Route path="/editor" component={CourseEditor}/>*/}
           <Route path="/editor" render={(props) => <CourseEditor {...props}/>}/>
@@ -14,6 +17,7 @@ function App() {
             <CourseManager/>
             <CourseEditor/>
         </div>*/}
+          </div>
       </BrowserRouter>
   );
 }
