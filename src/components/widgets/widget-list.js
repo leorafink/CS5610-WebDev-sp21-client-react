b05e755a5e94c813cb40606a65f2815065ef62e7
leorafink1 <fink.le@husky.neu.edu>
2021-03-22 00:33:36 -0400

    widget list

--- src/components/widgets/widget-list.js
+++ src/components/widgets/widget-list.js
@@ -1,16 +1,19 @@
 import React, {useState, useEffect} from "react";
 import HeadingWidget from "./heading-widget";
 import ParagraphWidget from "./paragraph-widget";
+import {useParams} from "react-router-dom";
 
 const WidgetList = () => {
+    const {topicId} = useParams()
     const [widgets, setWidgets] = useState([])
     useEffect(() => {
-        fetch("http://localhost:8080/api/topics/ABC234/widgets")
+        fetch(`http://localhost:8080/api/topics/${topicId}/widgets`)
             .then(response => response.json())
             .then(widgets => setWidgets(widgets))
-    })
+    }, [topicId])
     return(
         <div>
+            <i className="fas fa-plus float-right fa-2x"></i>
             <h1>Widget List</h1>
             <ul className="list-group">
                 {
---
+++ src/services/widget-service.js
@@ -0,0 +1,38 @@
+
+
+const WIDGETS_URL = "https://wbdv-generic-server.herokuapp.com/api/leorafink2/topics"
+const two_URL = "https://wbdv-generic-server.herokuapp.com/api/leorafink2/widgets"
+
+export const findWidgetsForTopic = (tid) =>
+    fetch(`${WIDGETS_URL}/${tid}/widgets`)
+        .then(response => response.json())
+
+export const createWidget = (tid, widget) =>
+    fetch(`${WIDGETS_URL}/${tid}/widgets`, {
+        method: "POST",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const deleteWidget = (wid) =>
+    fetch(`${two_URL}/${wid}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json());
+
+export const updateWidget = (wid, widget) =>
+    fetch(`${two_URL}/${wid}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json());
+
+export default {
+    findWidgetsForTopic, createWidget, deleteWidget, updateWidget
+}
