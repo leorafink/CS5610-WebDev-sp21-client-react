7c9ded153d0fd25fecf1c8b36d0635a6536a1fd7
leorafink1 <fink.le@husky.neu.edu>
2021-04-12 23:54:27 -0400

    check or x not working

--- src/components/quizzes/questions/multiple-choice-question.js
+++ src/components/quizzes/questions/multiple-choice-question.js
@@ -3,9 +3,19 @@ import React from "react";
 const MultipleChoiceQuestion = ({question}) => {
     return(
         <div>
-            <h4>Multiple Choice</h4>
-            <h3>{question.title}</h3>
-            {question.question}
+            <h4>{question.question}</h4>
+            {question.correct}
+            {
+                question.choices.map((choice) => {
+                    return(
+                        <label>
+                            <input type="radio" name={question._id}/>
+                            {choice}
+                        </label>
+                    )
+                })
+            }
+
         </div>
     )
 }
--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 const TrueFalseQuestion = ({question}) => {
-    return(
+    const [answer, setAnswer] = useState(null);
+    return (
         <div>
-            <h4>True-False</h4>
-            <h3>{question.title}</h3>
-            {question.question}
+            <h4>
+                {question.question}
+                {
+                    answer === question.correct &&
+                    <i className="fas fa-check"></i>
+                }
+                {
+                    answer !== question.correct &&
+                    <i className="fas fa-times"></i>
+                }
+
+
+            </h4>
+            {question.correct}
+            <br/>
+            {JSON.stringify(answer)}
+            <br/>
+            <label><input
+                type="radio"
+                onClick={() => setAnswer(true)}
+                name={question._id}/>True</label>
+            <br/>
+            <label><input
+                type="radio"
+                onClick={() => setAnswer(false)}
+                name={question._id}/>False</label>
         </div>
     )
 }
